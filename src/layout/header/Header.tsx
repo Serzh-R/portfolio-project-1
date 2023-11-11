dc66fd689e64f382a91858a2767ae3b5948d1029
Serzh-R <daarksen2@gmail.com>
2023-11-12 00:08:22 +0500

    menu map refactor

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -3,13 +3,14 @@ import styled from "styled-components";
 import {Logo} from "../../components/logo/Logo";
 import {Menu} from "../../components/menu/Menu";
 
+const items = ["Home", "About", "Latest Works", "Testimonials", "Contact"];
 
 export const Header = () => {
     return (
         <StyledHeader>
             <HeaderContainer>
                 <Logo/>
-                <Menu/>
+                <Menu menuItems={items}/>
             </HeaderContainer>
         </StyledHeader>
     );
